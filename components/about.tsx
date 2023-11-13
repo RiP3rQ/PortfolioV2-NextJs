a5d119e409d7cdfb0134eec2c25df588877b7c44
RiP3rQ <rafalpompa2000@gmail.com>
2023-11-13 19:58:11 +0100

    about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,42 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
+import { aboutData } from "@/lib/data";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+      className="flex flex-col relative h-screen text-center md:text-left
+    md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        O mnie
+      </h3>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1.5 }}
+        src={aboutData.profilePic}
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover 
+        md:rounded-lg md:w-64 md:h-96"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Tutaj znajdziesz{" "}
+          <span className="underline decoration-[#F7AB0A]/50">kilka</span>{" "}
+          informacji na mój temat
+        </h4>
+        <p className="text-base">{aboutData.backgroundInfo}</p>
+      </div>
+    </motion.div>
+  );
 };
 
 export default About;
--- lib/data.ts
+++ lib/data.ts
@@ -20,3 +20,15 @@ export const heroData = {
   img: "/images/hero.png",
   role: "<FullStack Developer/>",
 };
+
+export const aboutData = {
+  backgroundInfo: `Cześć, jestem Rafał Pompa i chcę zostać Full Stack Developerem. Mimo braku komeryjnego 
+    doświadczenia, posiadam już dość dużą wiedzę w zakresie programowania, którą staram się ciągle rozwija. 
+    Jestem osobą zdyscyplinowaną, rządną nowych wyzwań i nieustannie poszukującą nowej wiedzy. 
+    Dzięki temu łatwo przyswajam nowe umiejętności oraz technologie. Moją siłą jest kreatywność 
+    i umiejętność pracy w zespole. Cenię sobie zdrowy tryb życia i regularne ćwiczenia na siłowni, 
+    co pomaga mi zachować równowagę między pracą a życiem prywatnym. Jestem osobą bezpośrednią, 
+    która nie ma problemów z komunikacją. Jestem przekonany, że moja pasja do programowania i 
+    ciągłe dążenie do samodoskonalenia będą stanowić wartość dodaną dla każdego zespołu.`,
+  profilePic: "/images/hero.png",
+};
